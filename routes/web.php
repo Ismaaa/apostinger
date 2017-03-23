ccd48e61821d8dbb352225974fc8de1d2c1dce49
Ismaaa <didouhismael@gmail.com>
2017-03-23 16:17:07 +0100

    Modificades rutes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function (){
+       return view('welcome'); 
+    })->name('home');
+    
+    Route::get('principal', function() {
+        return view('principal');
+    });
+    
+    Route::get('welcome', function() {
+        return view('welcome');
+    });
+    
+    Route::get('admin', function() {
+        return view('admin');
+    });
 });
